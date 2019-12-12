b553e00c43aaa8abae5dcd000fd6cf109fec332a
RedGalaxySoftware <onankman@gmail.com>
2019-12-12 19:29:07 +0100

    Updated copyrights

--- src/main/java/net/rgsw/noise/InverseFractalPerlin3D.java
+++ src/main/java/net/rgsw/noise/InverseFractalPerlin3D.java
@@ -1,10 +1,6 @@
 /*
  * Copyright (c) 2019 RGSW
- * All rights reserved. Do not distribute.
- * This file is part of the Modernity, and is licensed under the terms and conditions of RedGalaxy.
- *
- * Date:   11 - 14 - 2019
- * Author: rgsw
+ * Licensed under Apache 2.0 license
  */
 
 package net.rgsw.noise;
